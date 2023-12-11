eb2c9a6e4ef35b87706a541b13e1703261914e39
AllRedCat <leonismogabriel@gmail.com>
2023-12-11 03:27:20 -0300

    add calculator app project

---
+++ calculator-app/script.js
@@ -0,0 +1,28 @@
+// switch theme
+document.addEventListener('DOMContentLoaded', function () {
+    const themeOptions = document.querySelectorAll('.theme-option');
+
+    themeOptions.forEach(option => {
+        option.addEventListener('click', function () {
+            const selectedTheme = this.getAttribute('data-theme');
+            document.documentElement.setAttribute('data-theme', selectedTheme);
+        })
+    })
+})
+
+// calculadora
+function appendToResult(value) {
+    document.getElementById('result').value += value;
+}
+
+function calculateResult() {
+    try {
+        document.getElementById('result').value = eval(document.getElementById('result').value);
+    } catch (error) {
+        document.getElementById('result').value = 'Error';
+    }
+}
+
+function clearResult() {
+    document.getElementById('result').value = '';
+}
